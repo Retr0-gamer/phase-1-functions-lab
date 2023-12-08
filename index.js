dfb37a6639a1e10dac2f2e4820774b11b6d7ac77
+ <salimhetham@gmail.com>
2023-12-09 02:50:31 +0300

    practise

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+    if (value >= 42) {
+        return value - 42
+    } else if (value <= 42) {
+        return 42 - value
+    };
+}
+
+function distanceFromHqInFeet(value) {
+    return distanceFromHqInBlocks(value) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264
+    } else
+        return (destination - start) * 264
+
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceFromHqInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+    } else if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) <= 2500 && distanceFromHqInFeet(start, destination) > 2000) {
+        return 25
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far"
+    }
+}
